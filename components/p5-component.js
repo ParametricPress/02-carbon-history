da40b49b45d0b4d496a285b20145c71a818cce21
Aatish Bhatia <aatish@gmail.com>
2020-04-10 16:14:10 -0400

    p5 full width test

--- components/p5-component.js
+++ components/p5-component.js
@@ -1,50 +1,68 @@
 const React = require('react');
-const D3Component = require('idyll-d3-component');
 const p5 = require('p5');
 let p5sketch;
 
-class p5Component extends D3Component {
-  initialize(node, props) {
+class p5Component extends React.Component {
 
-    console.log(node);
-    console.log(node.clientWidth, node.offsetWidth);
+  initialize(node, props) {
 
     const sketch = ( p ) => {
 
-      p.setup = () => {
+      p.setup = function() {
         p.createCanvas(600, 600);
-        p.background('pink');
         p.textAlign(p.CENTER, p.CENTER);
         p.textSize(20);
-        p.text('p5 hooray!', p.width/2, p.height/2);
-        //p.noLoop();
       };
 
-      p.draw = () => {
+      p.draw = function() {
         p.circle(p.mouseX, p.mouseY, 50);
       };
 
+      p.onResize = function() {
+      }
+
       p.onUpdate = function(props, oldProps) {
-        p.background(p.random(255), p.random(255), p.random(255));
-        p.textAlign(p.CENTER, p.CENTER);
-        p.textSize(20);
-        p.text('I was updated!', p.width/2, p.height/2);
+
+        /* this is a hack to resize the p5 sketch once its containing conditional becomes visible */
+        if (props.isVisible !== oldProps.isVisible) {
+
+          setTimeout(function() {
+            let newWidth = document.getElementById('firstsketch').offsetWidth
+            p.resizeCanvas(newWidth, 600);
+            p.background(p.random(255), p.random(255), p.random(255));
+            p.text('I was updated!', p.width/2, p.height/2);
+          }, 50);
+
+        } else {
+
+          p.background(p.random(255), p.random(255), p.random(255));
+          p.text('I was updated!', p.width/2, p.height/2);
+
+        }
+
       };
 
     };
 
     p5sketch = new p5(sketch, node);
 
-    //console.log(node);
-    //console.log(props);
-
   }
 
-  update(props, oldProps) {
-    //console.log('Updating component properties', props, oldProps);
-    p5sketch.onUpdate(props, oldProps);
-
+  componentWillReceiveProps(newProps) {
+    p5sketch.onUpdate(newProps, this.props);
   }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    const { className, id, style } = this.props;
+    return (
+      <div ref={(node) => { this.initialize(node, this.props) }} className={className} id={id} style={Object.assign({ width: '100%'}, style)} />
+    );
+  }
+
 }
 
 module.exports = p5Component;
