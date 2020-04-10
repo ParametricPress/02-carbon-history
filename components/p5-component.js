bddd7c06cfb2982f217ab4e5ec136de83c38c89c
Aatish Bhatia <aatish@gmail.com>
2020-04-10 11:17:51 -0400

    p5 onUpdate test

--- components/p5-component.js
+++ components/p5-component.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const D3Component = require('idyll-d3-component');
 const p5 = require('p5');
+let p5sketch;
 
 class p5Component extends D3Component {
   initialize(node, props) {
@@ -10,25 +11,38 @@ class p5Component extends D3Component {
       let y = 100;
 
       p.setup = () => {
-        p.createCanvas(400, 400);
+        p.createCanvas(600, 600);
         p.background('pink');
         p.textAlign(p.CENTER, p.CENTER);
-        p.text('p5 hooray!', 200, 200);
+        p.textSize(20);
+        p.text('p5 hooray!', p.width/2, p.height/2);
         p.noLoop();
       };
 
       p.draw = () => {
       };
 
+      p.onUpdate = function(props, oldProps) {
+        p.background(p.random(255), p.random(255), p.random(255));
+        p.textAlign(p.CENTER, p.CENTER);
+        p.textSize(20);
+        p.text('I was updated!', p.width/2, p.height/2);
+        p.noLoop();
+      };
+
 
     };
 
-    let myp5 = new p5(sketch, node);
+    p5sketch = new p5(sketch, node);
+
+    //console.log(node);
+    //console.log(props);
 
   }
 
   update(props, oldProps) {
     //console.log('Updating component properties', props, oldProps);
+    p5sketch.onUpdate(props, oldProps);
 
   }
 }
