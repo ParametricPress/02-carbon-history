37bdaddb62967da83882a4d10de62feb210a989b
Aatish Bhatia <aatish@gmail.com>
2020-04-21 15:20:18 -0400

    updated text, graph test

--- components/plotly-component.js
+++ components/plotly-component.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const D3Component = require('idyll-d3-component');
-const Plotly = require('plotly.js');
+const Plotly = require('plotly.js-basic-dist-min');
 
 class plotlyComponent extends D3Component {
 
@@ -20,9 +20,13 @@ class plotlyComponent extends D3Component {
     //console.log('plotly props changed');
 
      // this is a hack to resize the plotly graph once its containing conditional becomes visible
-    if (props.isVisible !== oldProps.isVisible) {setTimeout(() => Plotly.newPlot(this.state.node, props.data, props.layout, props.config), 50); }
+    if (props.isVisible !== oldProps.isVisible) {
+      console.log('creating Plotly graph');
+      setTimeout(() => Plotly.newPlot(this.state.node, props.data, props.layout, props.config), 50);
+    }
     else {
       // manually update graph on props change
+      console.log('updating Plotly graph');
       Plotly.react(this.state.node, props.data, props.layout, props.config);
     }
 
