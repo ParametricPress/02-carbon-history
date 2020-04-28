a5e675dd639c432a7f1aa5907edda0184660be86
Aatish Bhatia <aatish@gmail.com>
2020-04-28 18:03:01 -0400

    first attempt at rescaling annotation size
    as you travel further along the graph, the older annotations get smaller and eventually disappear.

--- context.js
+++ context.js
@@ -1,6 +1,9 @@
 const getCO2Level = (data, year) => data.filter(e => e.year == year)[0].ppm;
 const getCO2Fraction = (data, year) => (getCO2Level(data, '2019') - getCO2Level(data, year)) / (getCO2Level(data, '2019') - 280);
-const makeAnnotation = (data, year, annotationText, deltaX, deltaY, xanchor, yanchor) => ({
+const map = (n, start1, stop1, start2, stop2) => Math.min(Math.max((n - start1) / (stop1 - start1) * (stop2 - start2) + start2, start2),stop2);
+const makeAnnotation = (data, graphYear, year, annotationText, deltaX, deltaY, xanchor, yanchor) => (
+                        (20 * Math.pow((2020 - year)/(2020 - graphYear), 0.5) > 7) ?
+                        {
                           x: year,
                           y: getCO2Level(data, year) + 0.2,
                           xref: 'x',
@@ -8,11 +11,15 @@ const makeAnnotation = (data, year, annotationText, deltaX, deltaY, xanchor, yan
                           xanchor: xanchor ? xanchor : 'center',
                           yanchor: yanchor ? yanchor : 'bottom',
                           text: annotationText,
+                          font: {
+                            size: 20 * Math.pow((2020 - year)/(2020 - graphYear), 0.5),
+                          },
                           showarrow: true,
-                          arrowhead: 2,
-                          ax: !isNaN(deltaX) ? deltaX : -25,
-                          ay: !isNaN(deltaY) ? -deltaY : -100
-                        });
+                          arrowhead: 2 * Math.pow((2020 - year)/(2020 - graphYear), 0.5),
+                          ax: (!isNaN(deltaX) ? deltaX : -25 ) * Math.pow((2020 - year)/(2020 - graphYear), 0.5),
+                          ay: (!isNaN(deltaY) ? -deltaY : -100) * Math.pow((2020 - year)/(2020 - graphYear), 0.5)
+                        } : NaN
+                        );
 
 module.exports = (ctx) => {
 
