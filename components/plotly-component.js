247cbba46cd9eeecf4c5826e5c7df29aa9d78f30
Aatish Bhatia <aatish@gmail.com>
2020-05-18 15:28:28 -0400

    style changes

--- components/plotly-component.js
+++ components/plotly-component.js
@@ -36,6 +36,12 @@ class plotlyComponent extends D3Component {
 
     if (width > 0) {
       //console.log('Plotly Graph Created', this.props);
+      let height = width/1.618;
+      let viewportHeight = window.innerHeight;
+      if (height > viewportHeight) {
+        // ensure that graph doesn't take up more than 85% of screen height
+        width = 0.85 * viewportHeight * 1.618;
+      }
       Plotly.newPlot(this.state.node, this.props.data, this.props.layout(width));
 
       this.setState((state) => {
@@ -53,6 +59,12 @@ class plotlyComponent extends D3Component {
     if (this.state.graphCreated) {
       //console.log('Plotly Graph Resized');
       let width = this.state.node.getBoundingClientRect().width;
+      let height = width/1.618;
+      let viewportHeight = window.innerHeight;
+      if (height > viewportHeight) {
+        // ensure that graph doesn't take up more than 85% of screen height
+        width = 0.85 * viewportHeight * 1.618;
+      }
       Plotly.react(this.state.node, this.props.data, this.props.layout(width));
 
       this.setState((state) => {
