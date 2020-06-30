bd73e895bdb1b1c261641cb1b0a12530b80a150e
Matthew Conlen <matthew.conlen@nytimes.com>
2020-06-29 22:35:24 -0700

    update styles

---
+++ components/end-matter.js
@@ -0,0 +1,23 @@
+const React = require('react');
+
+class EndMatter extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { hasError, idyll, updateProps, clickCount, ...props } = this.props;
+    return (
+      <div style={{margin: props.hasFinished ? '2em 0' : '50vh 0'}} >
+        {
+          props.hasFinished ?
+            '' :
+            'You haven\'t finished yet'
+        }
+      </div>
+    );
+  }
+}
+
+module.exports = EndMatter;
